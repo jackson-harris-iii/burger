fae0f3875631fa5b44e4b89642a4459c77907cba
Apollo <jackson.harris3@gmail.com>
2018-04-27 00:57:44 -0700

    create db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Western');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Garden', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Western W/Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
+INSERT INTO burgers (burger_name) VALUES ('Bacon Jalepno w/Cheese');
