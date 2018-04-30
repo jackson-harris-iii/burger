551387159631e3ed2bc0d42472607ca58b641021
Apollo <jackson.harris3@gmail.com>
2018-04-30 13:11:24 -0700

    update db credentials

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    port: process.env.port || 8889,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+	port: 3306,
+	host: 's554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'm6zaotf4s1gafc25',
+	password: 'z886a72kon9sifw7',
+	database: 'dlbc5v3bfxbntng4',
 });
 
 // Make connection.
